ac40bb8ce5f3a424ee42cdd044954e24d449e304
zarmeen92 <Zarmeen@phd-research-02.iba.edu.pk>
2021-11-08 15:22:35 +0500

    update

---
+++ demowebsite/myweb.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   #return 'Hello World'
+   return render_template('index.html')
+
+@app.route('/second/')
+def second():
+    return render_template('second.html')
+if __name__ == '__main__':
+   app.run()
