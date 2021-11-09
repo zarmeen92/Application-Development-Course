8292bdb4f3f63d471763a1c2da36354a472c3bdd
zarmeen92 <Zarmeen@phd-research-02.iba.edu.pk>
2021-11-09 15:23:47 +0500

    update

--- demowebsite/myweb.py
+++ demowebsite/myweb.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,5 +9,15 @@ def index():
 @app.route('/second/')
 def second():
     return render_template('second.html')
+
+@app.route('/marks/')
+def marks():
+    return render_template('marks.html')
+
+@app.route('/submitform/',method=['post'])
+def submitform():
+	result = request.form
+	return render_template('result.html',result=result)
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
