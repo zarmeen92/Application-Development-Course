18d0417b9d0613f3b6a3e9d9d72841109c435b11
zarmeen92 <Zarmeen@phd-research-02.iba.edu.pk>
2021-12-03 10:01:25 +0500

    update

---
+++ dashmultipage/apps/app2.py
@@ -0,0 +1,10 @@
+from dash import html
+from dash import dcc
+
+
+layout = html.Div([
+    html.H3('App 2')
+    
+])
+
+
---
+++ dashmultipage/index.py
@@ -0,0 +1,29 @@
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+
+from app import app
+from apps import app1, app2
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+	dcc.Link('Go to App 1', href='/apps/app1'),
+	html.Hr(),
+	dcc.Link('Go to App 2', href='/apps/app2'),
+    html.Div(id='page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == '/apps/app1':
+        return app1.layout
+    elif pathname == '/apps/app2':
+        return app2.layout
+    else:
+        return 'This is the home screen'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
